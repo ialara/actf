6f14d26c36345fcc24340edff133b1bf455e954e
Ignacio Lara <ilara@prgs.edu>
2022-09-16 17:10:13 -0700

    modified test cases for test_scheduler to include duplicate values

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -21,18 +21,20 @@ class MockStudent:
     def get_upgrade(self):
         return self.ug
         
-days = [1,     10,     3,     4,      2,     9]
-ugs = ['IPUG','IPUG', 'FLUG', 'MQT', 'FLUG', 'MQT']
+days = [1,     10,     3,     4,      2,     9,    9,      2]
+ugs = ['IPUG','IPUG', 'FLUG', 'MQT', 'FLUG', 'MQT', 'MQT', 'FLUG']
 days_sorted = sorted(days)
 
 ## expected for MQT>IPUG>FLUG priorities and given days/ugs:
 # [('MQT', 4) <- [3],
 #  ('MQT', 9) <- [5],
+#  ('MQT', 9) <- [6],
 #  ('IPUG', 1) <- [0],
 #  ('IPUG', 10) <- [1],
 #  ('FLUG', 2) <- [4],
-#  ('FLUG', 3) <- [2]]
-## so UG-date priority of pilot id should be: [3, 5, 0, 1, 4, 2]
+#  ('FLUG', 2) <- [7],
+#  ('FLUG', 3) <- [2]
+## so UG-date priority of pilot id should be: [3, 5, 6, 0, 1, 4, 7, 2]
 
 @pytest.fixture
 def my_students():
@@ -65,13 +67,13 @@ def test_upgrades_assigned_scheduling_priority(my_students, my_scheduler, ug_pri
     assert actual == expected
     
 def test_students_prioritized_by_ug(my_students, my_scheduler, ug_priorities):
-    expected_ids = [3, 5, 0, 1, 2, 4] # Matches in UG are left in order they were passed
+    expected_ids = [3, 5, 6, 0, 1, 2, 4, 7] # Matches within UG are left in order they were passed
     prioritized = my_scheduler.prioritize_students_by_ug(my_students, ug_priorities)
     actual_ids = [student.id for student in prioritized]
     assert actual_ids == expected_ids
     
 def test_scheduling_philosophy_upgrade_then_date(my_students, my_scheduler, ug_priorities):
-    expected_ids = [3, 5, 0, 1, 4, 2] # See above
+    expected_ids = [3, 5, 6, 0, 1, 4, 7, 2] # See above. Matches with same ug/date are left in order passed
     prioritized = my_scheduler.prioritize_students_by_flight_date(my_students)
     prioritized = my_scheduler.prioritize_students_by_ug(prioritized, ug_priorities)
     actual_ids = [student.id for student in prioritized]
